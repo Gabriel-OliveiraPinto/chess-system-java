6950e5c01e9533ffc9eca11ae5699f7fd4433cf6
Gabriel Pinto <gabrielsnaker@gmail.com>
2023-11-29 10:59:25 -0300

    "Starting to implement Board and Piece"

--- src/com/udemy/Main.java
+++ src/com/udemy/Main.java
@@ -1,12 +1,13 @@
 package com.udemy;
 
+import com.udemy.boardgame.Board;
 import com.udemy.boardgame.Position;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+       Board board = new Board(8,8);
+
     }
 }
